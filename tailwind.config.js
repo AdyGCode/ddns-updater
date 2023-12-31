fbdf2487cbcd9f964455e9b8d38339b570fcb5a2
Ady G Code <ady.g.coder@gmail.com>
2023-12-31 15:55:26 +0800

    feat(UI): User interface refresh

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["**/*.html"], // This is where your HTML templates / JSX files are located
+    theme: {
+        extend: {
+
+        },
+    },
+    plugins: [],
+}
+
